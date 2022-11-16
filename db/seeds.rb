b900752dcec193de4565af9bd10021fb2e832a65
Dimitri Depardieu <dimitridepardieu@gmail.com>
2022-11-16 23:21:09 +0100

    Restaurants and Reviews controllers and views in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+aloha = { name: 'Aloha Bar', address: 'Hawaii', phone_number: '0633333333', category: 'belgian' }
+little = { name: 'Little Italy', address: 'Italy', phone_number: '0688888888', category: 'italian' }
+poke = { name: 'Poke Bar', address: 'Japan', category: 'japanese' }
+chouette = { name: 'La Chouette', address: 'Dijon', phone_number: '0399999999', category: 'french' }
+jura = { name: 'Le Jura', address: 'Jura', phone_number: '0477777777', category: 'french' }
+
+[aloha, little, poke, chouette, jura].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
